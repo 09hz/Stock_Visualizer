aa6bb8bd470570a10d9ec42047d046ebf31d68a8
Rodrigo Jesus Garcia Antonio <jesusantonio1223123@gmail.com>
2026-04-25 11:34:05 -0400

    Commented out  sma lines to test yf data

--- Live/live-chart.py
+++ Live/live-chart.py
@@ -11,10 +11,10 @@ if __name__ == '__main__':
     df = msft.history(period="1y", interval="1d")
 
     #Indicator Values
-    #sma = df.ta.sma(length=20).to_frame()
-    #sma = sma.reset_index()
-    #sma = sma.rename(columns={"Date" : "time", "SMA_20" :"value" })
-    #sma = sma.dropna()
+    sma = df.ta.sma(length=20).to_frame()
+    sma = sma.reset_index()
+    sma = sma.rename(columns={"Date" : "time", "SMA_20" :"value" })
+    sma = sma.dropna()
 
 
     df = df.reset_index()
@@ -34,8 +34,8 @@ if __name__ == '__main__':
     chart.set(df)
 
     #SMA Line
-    #line = chart.create_line()
-    #line.set(sma)
+    line = chart.create_line()
+    line.set(sma)
 
     chart.watermark("MSFT")
     chart.show(block = True)
